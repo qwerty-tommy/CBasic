20832075af372172e10a6616fed3e789ec8712e8
qwertyou <chjw1346@gmail.com>
2023-07-08 08:06:14 +0900

    트리,트리순회 - 예제끝

--- Pointer/main_Tree.cpp
+++ Pointer/main_Tree.cpp
@@ -5,7 +5,7 @@
 #include "array_Stack.h"
 
 void makeTree(TreeNode* root) {
-	TreeNode* a = root, * b, * c, * d, * e, * f, * g;
+	TreeNode* a = root, * b, * c, * d, * e, * f, * g, * e1, * e2, * e3, *e4;
 
 	a->data = 'A';
 
@@ -27,6 +27,18 @@ void makeTree(TreeNode* root) {
 	g = (TreeNode*)malloc(sizeof(TreeNode));
 	g->data = 'G';
 
+	e1 = (TreeNode*)malloc(sizeof(TreeNode));
+	e1->data = 'E1';
+
+	e2 = (TreeNode*)malloc(sizeof(TreeNode));
+	e2->data = 'E2';
+
+	e3 = (TreeNode*)malloc(sizeof(TreeNode));
+	e3->data = 'E3';
+
+	e4 = (TreeNode*)malloc(sizeof(TreeNode));
+	e4->data = 'E4';
+
 	a->left = b;
 	a->right = c;
 	b->left = d;
@@ -35,12 +47,25 @@ void makeTree(TreeNode* root) {
 	c->right = g;
 	d->left = NULL;
 	d->right = NULL;
-	e->left = NULL;
-	e->right = NULL;
+	e->left = e1;
+	e->right = e2;
 	f->left = NULL;
 	f->right = NULL;
 	g->left = NULL;
 	g->right = NULL;
+
+	e1->left = NULL;
+	e1->right = NULL;
+
+	e2->left = e3;
+	e2->right = e4;
+
+	e3->left = NULL;
+	e3->right = NULL;
+
+	e4->left = NULL;
+	e4->right = NULL;
+
 }
 
 void pre_traverse(TreeNode* root) {
@@ -70,22 +95,46 @@ void in_traverse(TreeNode* root) {
 	init(&st);
 
 	TreeNode* cur = root;
-
-	do {
-		push(&st, cur);
-		if (cur->left)
-		{
-			cur = cur->left;
+	push(&st, cur);
+	while (!is_empty(&st)) {
+		while (root!=NULL){
+			push(&st, root);
+			root = root->left;
 		}
-		
+		printf("%c->", (root = pop(&st))->data);
+		root = root->right;
+	}
 
-	} while (1);
-
-	printf("\b\b");
+	printf("\b\b\b\b ");
 }
 
-void post_traverse(TreeNode* root) {
 
+#define MAX_SIZE 11
+void post_traverse(TreeNode* root) {
+	element buf[MAX_SIZE];
+	StackType st;
+	init(&st);
+
+	TreeNode* cur = root;
+	int i = 0;
+	do
+	{
+		if (cur != NULL) {
+			if (cur->left != NULL) push(&st, cur->left);
+			buf[i++] = cur;
+			cur = cur->right;
+		}
+		else {
+			if (is_empty(&st)) break;
+			cur = pop(&st);
+		}
+	} while (1);
+
+	for (int i = 0; i < MAX_SIZE; i++)
+	{
+		printf("%c->",buf[MAX_SIZE - i-1]->data);
+	}
+	printf("\b\b ");
 }
 
 int main() {
@@ -93,5 +142,7 @@ int main() {
 	root = (TreeNode*)malloc(sizeof(TreeNode));
 
 	makeTree(root);
-	pre_traverse(root);
+	//pre_traverse(root);
+	//in_traverse(root);
+	post_traverse(root);
 }
